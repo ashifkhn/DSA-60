99e9046c8fa94761f0c32472496c9083e779e3f8
Ashif Khan <ashifkhn02@gmail.com>
2024-01-04 17:12:09 +0530

    Swap alternate

--- js/1.SwapAlternate.js
+++ js/1.SwapAlternate.js
@@ -4,12 +4,12 @@ const swapAlternate =(arr)=>{
             let temp =arr[i];
             arr[i]=arr[i+1];
             arr[i+1]=temp;
+            // [arr[i], arr[i+1]]=[arr[i+1],arr[i]]
         }
     }
 
     return arr
 }
-
 const arr=[1,2,3,4,5,6,7,8]
 console.log( swapAlternate(arr));
 
